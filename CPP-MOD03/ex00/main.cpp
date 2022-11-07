82fcacd6bd48a6f739954aa32e00bdde6b5de972
root <root@LAPTOP-ERHSNE9O.localdomain>
2022-11-07 22:29:56 +0300

    some adjustments

--- CPP-MOD03/ex00/main.cpp
+++ CPP-MOD03/ex00/main.cpp
@@ -9,11 +9,13 @@ int main()
 	b.set_attack(5);
 	while(b.get_hp() && a.get_hp() && a.get_ep() && b.get_ep()) {
 		a.attack("Eyup");
-		b.takeDamage(a.get_attack());
-		b.beRepaired(a.get_attack() - 3);
+		if (a.get_ep() > 0 ||a.get_hp())
+			b.takeDamage(4);
+		b.beRepaired(3);
 		b.attack("Burak");
-		a.takeDamage(b.get_attack());
-		a.beRepaired(b.get_attack() - 3);
+		if (b.get_ep() && b.get_hp())
+			a.takeDamage(5);
+		a.beRepaired(3);
 		sleep(3);
 	}
 }
